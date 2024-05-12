d9f34ecf51be309175e6d132f497b61e49b846da
NahuelBMiras <nahuelbasailmiras@gmail.com>
2024-05-12 20:10:30 -0300

    pagina Receta sin terminar

---
+++ src/scripts/recepi.mjs
@@ -0,0 +1,228 @@
+const recipes = [
+  {
+    name: "pizza a la piedra",
+    ingredients: [
+      "1 kg harina 000",
+      "60 g levadura prensada",
+      "2 cucharadas y media de sal",
+      "2 cucharadas y media de azúcar",
+      "500 cc agua",
+      "6 cucharadas manteca derretida",
+      "Cantidad necesaria semolin",
+    ],
+    steps: [
+      [
+        "Activación de la levadura:",
+        "Desmenuza la levadura en un bowl y añade el azúcar.Mezcla bien.",
+        "Agrega 200 cc de agua tibia y revuelve hasta que esté homogéneo.",
+        "Incorpora 3 cucharadas de harina y mezcla nuevamente.",
+        "Cubre el recipiente con film transparente y un paño y deja reposar durante 20 minutos para activar la levadura.",
+      ],
+      [
+        "Preparación de la masa:",
+        "Forma una corona con la harina y coloca la sal alrededor, evitando el centro.",
+        "En el centro, agrega la levadura activada.",
+        "Mezcla todo y luego añade agua tibia poco a poco mientras amasas.",
+        "Agrega la manteca derretida y sigue amasando hasta que la masa tome consistencia.",
+        "Deja reposar la masa en un bowl tapado con film transparente y un paño durante 30 minutos en un lugar cálido para que fermente adecuadamente.",
+      ],
+      [
+        "Desgasificación y reposo adicional:",
+        "Desgasifica la masa y deja reposar durante 5 minutos más.",
+        "Enciende el horno y coloca una fuente invertida en la base del mismo para la cocción a la piedra.",
+      ],
+      [
+        "División y formado de las pizzas:",
+        "Divide la masa en seis bollos.",
+        "Espolvorea semolín sobre la mesada y estira la masa, dándole la medida aproximada de la bandeja del horno para evitar que se pegue.",
+      ],
+      [
+        "Cocción de las pizzas:",
+        "Una vez estiradas las masas y con el horno caliente, coloca la masa de la pizza sobre la fuente caliente en el horno.",
+        "Cocina durante aproximadamente 5 minutos. El tiempo puede variar según el horno.",
+      ],
+      [
+        "Finalización y montaje de las pizzas:",
+        "Retira solo la pizza con la ayuda de una pala.",
+        "Coloca la salsa y el queso sobre la pizza.",
+        "Coloca la pizza en un nivel superior para que se termine de cocinar y se derrita el queso.",
+      ],
+    ],
+    recipeImg: "url(../../public/Recetas/pizza.jpg)",
+    recipeVideo:
+      "https://www.youtube.com/embed/G3LmiPpjMgY?si=4pcii-ldTIPEHMZa",
+  },
+  {
+    name: "milanesas de soja",
+    ingredients: [
+      "1 taza de proteína texturizada de soja",
+      "1 taza de pan rallado",
+      "1 huevo (o sustituto de huevo para veganos)",
+      "2 dientes de ajo, picados",
+      "1 cucharadita de pimentón dulce",
+      "1 cucharadita de orégano",
+      "Sal y pimienta al gusto ",
+      "Aceite para freír",
+    ],
+    steps: [
+      [
+        "Rehidratación de la proteína texturizada de soja:",
+        "Coloca la proteína texturizada de soja en un bol y cúbrela con agua caliente.",
+        "Déjala reposar durante 10-15 minutos o hasta que se haya hidratado por completo.",
+        "Escurre bien la soja hidratada y presiónala para eliminar el exceso de agua.",
+      ],
+      [
+        "Preparación de la mezcla de condimentos:",
+        "En un bol grande, mezcla la proteína texturizada de soja hidratada con los dientes de ajo picados, el pimentón dulce, el orégano, la sal y la pimienta al gusto.",
+        "Asegúrate de que los condimentos estén bien distribuidos en la soja.",
+      ],
+      [
+        "Armado de las milanesas:",
+        "Toma porciones de la mezcla de soja y dales forma de milanesas del tamaño deseado.",
+        "Pasa cada milanesa por el huevo batido (o sustituto de huevo) y luego por el pan rallado, asegurándote de que estén bien cubiertas.",
+      ],
+      [
+        "Cocción de las milanesas:",
+        "Calienta suficiente aceite en una sartén grande a fuego medio.",
+        "Cuando el aceite esté caliente, coloca las milanesas de soja en la sartén y fríelas hasta que estén doradas por ambos lados, aproximadamente 3-4 minutos por cada lado.",
+        "Retira las milanesas del aceite y colócalas sobre papel absorbente para eliminar el exceso de grasa.",
+      ],
+    ],
+    recipeImg: "url(../../public/Recetas/Milanesas.png)",
+    recipeVideo:
+      "https://www.youtube.com/embed/G3LmiPpjMgY?si=3CV52zT4a-m5e7jc",
+  },
+  {
+    name: "pan de campo",
+    ingredients: [
+      "10 gramos de levadura seca",
+      "500 gramos de harina común",
+      "10 gramos de sal fina",
+      "30 gramos de grasa",
+      "300 ml de agua tibia",
+    ],
+    steps: [
+      ["Fermento:", "Mezclar la levadura con la harina y agregar el agua."],
+      ["Masa:", "Tamizar la harina."],
+      [
+        "Agregar la sal con el azúcar para volver a mezclarlo.",
+        "Luego la grasa para integrarlo con la harina hasta que se disuelva.",
+      ],
+      ["Agregar el fermento más el agua, para empezar a unir todo muy bien."],
+      [
+        "Después de haber formado una masa lisa, se debe llevar a un bowl para que duplique volumen.Unos 30 minutos.",
+      ],
+      [
+        "Cuando haya duplicado volumen se debe armar un cilindro con la masa. Para cortar del tamaño deseado",
+        "Armar los bollos y llevar a una bandeja aceitada.",
+        "Se debe cortar los panes y dejar que dupliquen su tamaño.",
+      ],
+      [
+        "Cuando haya duplicado su tamaño se debe llevar a un horno precalentado a 180 grados por 25 minutos aproximadamente",
+      ],
+    ],
+    recipeImg: "url(../../public/Recetas/Pan.jpg)",
+    recipeVideo:
+      "https://www.youtube.com/embed/TisLykAy050?si=da3BXNqJnm0Odd2w",
+  },
+  {
+    name: "risotto",
+    ingredients: [
+      "150 g (crudo) arroz yamaní integral",
+      "3/4 copa vino",
+      "1 1/2 taza caldo aprox",
+      "4-5 champis u hongo a elección",
+      "1/2 zucchini",
+      "1 zanahoria chica",
+      "1 cebolla chica",
+      "1/2 pimiento",
+      "2 cucharadas pasta de ajo",
+      "Opcional: queso",
+    ],
+    steps: [
+      [
+        "Preparación del arroz:",
+        "Después de haber remojado el arroz durante toda la noche, escúrrelo y enjuágalo bien bajo agua fría. Esto ayuda a eliminar el exceso de almidón y asegura que el risotto tenga la textura adecuada.",
+      ],
+      [
+        "Preparación de los ingredientes:",
+        "Pica finamente todas las verduras: la cebolla, el pimiento, la zanahoria y el zucchini.",
+        "Corta los champiñones u hongos en rodajas.",
+      ],
+      [
+        "Cocción del risotto:",
+
+        "En una olla grande, calienta un poco de aceite de oliva a fuego medio.",
+        " Agrega la cebolla picada y saltea hasta que esté transparente.",
+        "Añade el arroz yamaní integral escurrido y remueve durante unos minutos hasta que esté ligeramente tostado y cubierto con el aceite.",
+        "Agrega la pasta de ajo y mezcla bien.",
+        "Vierte el vino blanco y cocina hasta que se haya absorbido por completo, removiendo ocasionalmente.",
+        "Añade las verduras picadas (zanahoria, pimiento, zucchini) y los champiñones cortados. Cocina por unos minutos hasta que las verduras estén tiernas.",
+        "Ahora, comienza a agregar el caldo caliente de verduras poco a poco, una taza a la vez, mientras revuelves constantemente. Asegúrate de que el arroz absorba cada adición de caldo antes de agregar más. Continúa este proceso hasta que el arroz esté cocido pero aún al dente, esto puede tomar alrededor de 20-25 minutos.",
+      ],
+      [
+        "Acabado y presentación:",
+
+        "Una vez que el arroz esté cocido al punto deseado, retira la olla del fuego.",
+        "Si lo deseas, puedes añadir un poco de queso rallado en este punto para darle un toque extra de cremosidad.",
+        "Sirve el risotto caliente, espolvoreado con un poco más de queso rallado si lo prefieres, y acompáñalo con un buen vino blanco.",
+      ],
+    ],
+    recipeImg: "(../../public/Recetas/risoto.jpg",
+    recipeVideo:
+      "https://www.youtube.com/embed/lNpqHQvrv5s?si=J0TP0tvpWZwDx5Ig",
+  },
+  //   {
+  //     name: "torta",
+  //     ingredients: [],
+  //     steps: ["paso 1", "paso 2", "paso 3"],
+  //     recipeImg: "url",
+  //   },
+  //   {
+  //     name: "budin",
+  //     ingredients: [],
+  //     steps: ["paso 1", "paso 2", "paso 3"],
+  //     recipeImg: "url",
+  //   },
+  //   {
+  //     name: "brownie",
+  //     ingredients: [],
+  //     steps: ["paso 1", "paso 2", "paso 3"],
+  //     recipeImg: "url",
+  //   },
+  //   {
+  //     name: "galletitas de avena y banana",
+  //     ingredients: [],
+  //     steps: ["paso 1", "paso 2", "paso 3"],
+  //     recipeImg: "url",
+  //   },
+  //   {
+  //     name: "vitel tone",
+  //     ingredients: [],
+  //     steps: ["paso 1", "paso 2", "paso 3"],
+  //     recipeImg: "url",
+  //   },
+  //   {
+  //     name: "sopa",
+  //     ingredients: [],
+  //     steps: ["paso 1", "paso 2", "paso 3"],
+  //     recipeImg: "url",
+  //   },
+  //   {
+  //     name: "pato a la naranja",
+  //     ingredients: [],
+  //     steps: ["paso 1", "paso 2", "paso 3"],
+  //     recipeImg: "url",
+  //   },
+];
+
+function searchRecepiName(nameRecepi) {
+  for (const recepi of recipes) {
+    if (recepi.name === nameRecepi) {
+      return recepi.name;
+    }
+  }
+  return null;
+}
+
+console.log(searchRecepiName("pan de campo"));
